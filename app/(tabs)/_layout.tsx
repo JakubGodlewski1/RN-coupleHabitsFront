02c6ca7f71327ba68b0a5a76b235b0db8004b724
Jakub Godlewski <jakubgodlewski50@gmail.com>
2024-07-09 12:16:06 +0200

    create tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,42 @@
 import {Tabs} from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
+import {Platform} from "react-native";
+import {Shadows} from "@/styles/Shadows";
 
 export default function TabsLayout(){
 
     return <Tabs
+        screenOptions={{
+            tabBarStyle: Platform.OS==="android" ? {height:76, paddingTop:6}:{height:100, paddingTop: 12},
+            tabBarItemStyle:{ ...Shadows.md, elevation:4, marginHorizontal: 10, paddingBottom:5, borderRadius: 8, marginBottom: Platform.OS === "android" ? 8 : 0},
+            tabBarInactiveBackgroundColor: Platform.OS === "android" ? "white":"transparent",
+            tabBarActiveBackgroundColor:"#EBF4CA",
+            headerShown: false,
+            tabBarLabelStyle:{fontFamily:"AbhayaLibre-SemiBold", color:"#000", fontSize: 13}
+        }}
         sceneContainerStyle={{backgroundColor:"transparent"}}
         initialRouteName="dashboard"
-        screenOptions={{
-            headerShown:false
-        }}
     >
         <Tabs.Screen
             name="ideas"
+            options={{
+                title:"Ideas",
+                tabBarIcon: ()=><Ionicons name="bulb-outline" size={24}/>
+            }}
         />
           <Tabs.Screen
             name="dashboard"
+            options={{
+                title:"Dashboard",
+                tabBarIcon:()=><Ionicons name="people-outline" size={24}/>
+            }}
         />
           <Tabs.Screen
             name="settings"
+            options={{
+                title:"Settings",
+                tabBarIcon:()=><Ionicons name="settings-outline" size={22}/>
+            }}
         />
 
     </Tabs>
---
+++ styles/Shadows.ts
@@ -0,0 +1,36 @@
+export const Shadows = {
+    md: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+    },
+    sm:
+        {
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.15,
+            shadowRadius: 1.84,
+        },
+}
+
+export const glassContainer = {
+    shadowColor: "#000",
+        shadowOffset: {
+        width: 2,
+            height: 4,
+    },
+    shadowOpacity: 0.2,
+        shadowRadius: 4,
+
+        elevation: 4,
+        borderRadius: 8,
+        borderColor: "#fff",
+        borderWidth: 1
+}
