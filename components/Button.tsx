833fdbcac1022546fdfca863678d152818a89609
Jakub Godlewski <jakubgodlewski50@gmail.com>
2024-07-18 10:14:24 +0200

    update button with extra types/looks

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,11 +3,11 @@ import {ReactNode} from "react"
 import {Shadows} from "@/styles/Shadows";
 import Text from "@/components/Text";
 
-type BtnType = "primary" | "secondary" | "tertiary" | "skip"
+type BtnType = "primary" | "secondary" | "tertiary" | "skip" | "white"
 
 type Props = {
     type?: BtnType,
-    onPress: ()=>void,
+    onPress: () => void,
     children?: ReactNode,
     iconPosition?: "left" | "right",
     title: String,
@@ -16,32 +16,38 @@ type Props = {
     }
 }
 
-const typeStylesMap: Record<BtnType, { bg:String, text:String }> = {
+const typeStylesMap: Record<BtnType, { bg: String, text: String }> = {
     primary: {
-        bg:"bg-primary",
-        text:"text-white"
+        bg: "bg-primary",
+        text: "text-white"
     },
-     secondary: {
-        bg:"bg-secondary",
-        text:"text-white"
+    secondary: {
+        bg: "bg-secondary",
+        text: "text-white"
     },
-     tertiary: {
-        bg:"bg-tertiary",
-        text:"text-white"
+    tertiary: {
+        bg: "bg-tertiary",
+        text: "text-white"
     },
-     skip: {
-        bg:"bg-skip",
-        text:"text-black"
+    skip: {
+        bg: "bg-skip",
+        text: "text-black"
     },
-
+    white: {
+        bg: "bg-white",
+        text: "text:black"
+    }
 }
 
-export default function Button({ children, iconPosition="left", title,  onPress, classNames, type="primary"}:Props) {
+export default function Button({children, iconPosition = "left", title, onPress, classNames, type = "primary"}: Props) {
 
-    return <TouchableOpacity  onPress={onPress} style={{...Shadows}} className={`flex-row  items-center justify-center space-x-5  rounded-[20px] p-4 ${typeStylesMap[type].bg} ${classNames?.wrapper}`}>
-        {iconPosition === "right" && <Text classNames={{text:`font-mainBold ${typeStylesMap[type].text}`}}>{title}</Text>}
-             {children && children}
-        {iconPosition === "left" && <Text classNames={{text:`font-mainBold ${typeStylesMap[type].text}`}}>{title}</Text>}
+    return <TouchableOpacity onPress={onPress} style={{...Shadows}}
+                             className={`flex-row  items-center justify-center space-x-5  rounded-[20px] p-4 ${typeStylesMap[type].bg} ${classNames?.wrapper}`}>
+        {iconPosition === "right" &&
+            <Text classNames={{text: `font-mainBold ${typeStylesMap[type].text}`}}>{title}</Text>}
+        {children && children}
+        {iconPosition === "left" &&
+            <Text classNames={{text: `font-mainBold ${typeStylesMap[type].text}`}}>{title}</Text>}
 
     </TouchableOpacity>
 
