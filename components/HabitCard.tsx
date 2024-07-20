1aabdcf983c3d4f41440433b34256be924894659
Jakub Godlewski <jakubgodlewski50@gmail.com>
2024-07-20 17:27:47 +0200

    add swipe up to habitcard

--- components/HabitCard.tsx
+++ components/HabitCard.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import {Alert, TouchableOpacity, View} from 'react-native';
-
+import {Alert, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Text from "@/components/Text";
 import {Habit} from "@/types";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import {Swipeable} from "react-native-gesture-handler";
+import {PanGestureHandler, Swipeable} from "react-native-gesture-handler";
 import {Shadows} from "@/styles/Shadows";
 import {Feather} from "@expo/vector-icons";
+import Animated, {useAnimatedStyle, useSharedValue, withSpring} from "react-native-reanimated";
 
 type Props = {
     habit: Habit,
@@ -21,6 +20,30 @@ const defaultOptions = {disabled: false, noCheckboxes: false};
 
 
 const HabitCard = ({habit, options = defaultOptions}: Props) => {
+    const translateY = useSharedValue(0);
+
+    const gestureHandler = (event: any) => {
+        if (event.nativeEvent.translationY < -20) {
+            translateY.value = withSpring(-50, {
+                damping: 20,
+                stiffness: 300,
+                mass: 0.5
+            })
+        } else {
+            translateY.value = withSpring(0,
+                {
+                    damping: 20,
+                    stiffness: 300,
+                    mass: 0.5
+                })
+        }
+    };
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{translateY: translateY.value}],
+        };
+    });
 
     const rightSwipe = () => {
         return <View style={{gap: 4}} className=" w-16 p-1">
@@ -36,48 +59,70 @@ const HabitCard = ({habit, options = defaultOptions}: Props) => {
         </View>
     }
 
-    return <View className="border-2 border-skip rounded-2xl">
+    return <View className="border-2 border-skip rounded-2xl overflow-hidden">
         <View style={{...Shadows}} className="m-2 rounded-2xl bg-white">
-            <Swipeable
-                renderRightActions={rightSwipe}
-            >
-                <View className="flex-row bg-white h-[88px] rounded-2xl">
-                    <TouchableOpacity
-                        activeOpacity={100}
-                        onPress={() => {
-                        }}
-                        className=" py-2 px-4 flex-1 bg-white rounded-l-xl items-center border-r-[0.2px]"
+            <View className="absolute bottom-0 w-[94%] mx-[3%] mb-1 flex-row border-t-[0.6px] border-skip pt-[2px]">
+                <Text classNames={{text: "mr-auto"}}>Strike: 🔥 <Text
+                    classNames={{text: "font-mainBold"}}>12</Text></Text>
+            </View>
+            <PanGestureHandler onGestureEvent={gestureHandler}>
+                <Animated.View style={[{overflow: "hidden"}, animatedStyle]}>
+                    <Swipeable
+                        renderRightActions={rightSwipe}
                     >
-                        <Text
-                            classNames={{text: "shrink text-center text-sm -ml-2 font-mainBold mb-auto"}}>{habit.details.mine.label}</Text>
-                        <BouncyCheckbox
-                            style={{marginRight: "auto"}}
-                            fillColor="#6EC166"
-                            disableBuiltInState={true}
-                            isChecked={habit.details.mine.completed}
-                            onPress={() => {
-                            }}
-                            size={20}
-                            innerIconStyle={{
-                                borderRadius: 4,
-                                borderColor: habit.details.mine.completed ? "#6EC166" : "gray"
-                            }}
-                            iconStyle={{borderRadius: 4}}
-                        />
-                    </TouchableOpacity>
-                    <View
-                        className={`p-2 flex-1 bg-white border-r-4 rounded-r-xl
+                        <View className="flex-row bg-white h-[88px] rounded-2xl ">
+                            <TouchableOpacity
+                                activeOpacity={100}
+                                onPress={() => {
+                                }}
+                                className=" py-2 px-4 flex-1 bg-white rounded-l-xl items-center border-r-[0.2px]"
+                            >
+                                <Text
+                                    classNames={{text: "shrink text-center text-sm -ml-2 font-mainBold mb-auto"}}>{habit.details.mine.label}</Text>
+                                <BouncyCheckbox
+                                    style={{marginRight: "auto"}}
+                                    fillColor="#6EC166"
+                                    disableBuiltInState={true}
+                                    isChecked={habit.details.mine.completed}
+                                    onPress={() => {
+                                    }}
+                                    size={20}
+                                    innerIconStyle={{
+                                        borderRadius: 4,
+                                        borderColor: habit.details.mine.completed ? "#6EC166" : "gray"
+                                    }}
+                                    iconStyle={{borderRadius: 4}}
+                                />
+                            </TouchableOpacity>
+                            <View
+                                className={`p-2 flex-1 bg-white border-r-4 rounded-r-xl
                      ${habit.details.partner.completed ? "border-secondary" : "border-primary"}`}
-                    >
-                        <Text
-                            classNames={{text: "shrink text-center text-sm font-mainBold"}}>{habit.details.partner.label}</Text>
-                    </View>
-                </View>
-            </Swipeable>
-
-
+                            >
+                                <Text
+                                    classNames={{text: "shrink text-center text-sm font-mainBold"}}>{habit.details.partner.label}</Text>
+                            </View>
+                        </View>
+                    </Swipeable>
+                </Animated.View>
+            </PanGestureHandler>
         </View>
     </View>
 };
 
 export default HabitCard;
+
+
+const styles = StyleSheet.create({
+    card: {
+        width: 300,
+        height: 400,
+        backgroundColor: 'lightblue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+});
